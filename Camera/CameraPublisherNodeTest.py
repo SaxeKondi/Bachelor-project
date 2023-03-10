0f7a726e41ba097b1e2b905054b68a2255a2b06f
Frederik Thrane <45735470+SaxeKondi@users.noreply.github.com>
2023-03-10 11:28:02 +0100

    Update CameraPublisherNodeTest.py

--- Camera/CameraPublisherNodeTest.py
+++ Camera/CameraPublisherNodeTest.py
@@ -24,16 +24,15 @@ class cameraPublisher:
         self.loop_rate = rospy.Rate(30)
 
         # Publishers
-        # self.pub = rospy.Publisher('cam_feed', Image, queue_size=10)
-        self.pub = rospy.Publisher('cam_feed', CompressedImage, queue_size=10)
+        self.pubComp = rospy.Publisher('cam_feed_comp', CompressedImage, queue_size=10)
+        self.pubRaw = rospy.Publisher('cam_feed_raw', Image, queue_size=10)
 
         # rospy.Subscriber('/cam_feed',Image,self.callback)
-        rospy.Subscriber('/cam_feed',CompressedImage,self.callback)
+        rospy.Subscriber('/cam_feed_comp',CompressedImage,self.callback)
 
 
 
     def callback(self, msg):
-        print("yo")
         # self.image = self.br.imgmsg_to_cv2(msg)
         self.image = self.br.compressed_imgmsg_to_cv2(msg)
 
@@ -41,8 +40,6 @@ class cameraPublisher:
 
     def start(self):
         while not rospy.is_shutdown():
-            print("hej")
-
             _, img = self.cap.read()
 
             if self.image is not None:
@@ -51,8 +48,8 @@ class cameraPublisher:
 
             if img is not None:
                 img = cv2.resize(img, (720,576))
-                # self.pub.publish(self.br.cv2_to_imgmsg(img))
-                self.pub.publish(self.br.cv2_to_compressed_imgmsg(img))
+                self.pubRaw.publish(self.br.cv2_to_imgmsg(img))
+                self.pubComp.publish(self.br.cv2_to_compressed_imgmsg(img))
 
             if cv2.waitKey(1) == ord('q'):
                 #exit while loop
