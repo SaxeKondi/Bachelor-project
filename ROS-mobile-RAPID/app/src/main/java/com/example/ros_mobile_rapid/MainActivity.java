1daca575bbf72d504ed7a07e761dbb97b8aec90c
nikolajtf <45765625+nikolajtf@users.noreply.github.com>
2023-03-10 11:30:24 +0100

    a

--- ROS-mobile-RAPID/app/src/main/java/com/example/ros_mobile_rapid/MainActivity.java
+++ ROS-mobile-RAPID/app/src/main/java/com/example/ros_mobile_rapid/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.ros_mobile_rapid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
