ceacc3beb7e6084921b4fbe4d267d5b59170c2e9
Frederik Thrane <45735470+SaxeKondi@users.noreply.github.com>
2023-05-24 19:13:09 +0200

    test code for ultransound feed with needle path

--- Camera/Outdated Code/CameraCapture.py
+++ Camera/Outdated Code/CameraCapture.py
@@ -6,25 +6,35 @@ def rescale_frame(frame, percent=75):
     dim = (width, height)
     return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
 
-cap = cv2.VideoCapture(4)
-pixelWidth = 1280
-pixelHeight = 720
+image = None
+cap = cv2.VideoCapture(0)  #Might need to be changed depending on which id the ultrasound scanner gets
+pixelWidth = 720
+pixelHeight = 480
+startx = int(pixelWidth/15)
+endx = int(pixelWidth - pixelWidth/20)
+starty = int(pixelHeight/9)
+endy = int(pixelHeight - pixelHeight/9)
+
+a = 12.25
+b = -2435.75
+startLinePoint = (int((0-b)/a),0)
+endLinePoint = (int(((endy-starty)-b)/a),(endy-starty))
+
 cap.set(3,pixelWidth)
 cap.set(4,pixelHeight)
 
 while True:
-    frameCaptured, frame = cap.read()
+    _, image = cap.read()
 
-    if(not frameCaptured):
-        break
+            
 
-    #frame = rescale_frame(img, 50)
-    cv2.imshow("Video Capture", frame)
-
-    #If q is pressed
-    if cv2.waitKey(1) == ord('q'):
-        #exit while loop
-        break
+    if image is not None:
+        image = image[starty:endy , startx:endx]
+        cv2.line(image, startLinePoint, endLinePoint, (0,0,255), 3)
+        cv2.imshow("ultrasound", image)
+        if cv2.waitKey(1) == ord('q'):
+            #exit while loop
+            break
 
 cap.release()
 cv2.destroyAllWindows()
