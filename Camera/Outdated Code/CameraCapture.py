e36d1837d408ae934122dbfdfcf98ffdaca7aa77
Frederik Thrane <45735470+SaxeKondi@users.noreply.github.com>
2023-05-27 15:15:40 +0200

    Updated camera cature to be able to save frames as png

--- Camera/Outdated Code/CameraCapture.py
+++ Camera/Outdated Code/CameraCapture.py
@@ -7,7 +7,7 @@ def rescale_frame(frame, percent=75):
     return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
 
 image = None
-cap = cv2.VideoCapture(0)  #Might need to be changed depending on which id the ultrasound scanner gets
+cap = cv2.VideoCapture(4)  #Might need to be changed depending on which id the ultrasound scanner gets
 pixelWidth = 720
 pixelHeight = 480
 startx = int(pixelWidth/15)
@@ -23,18 +23,29 @@ endLinePoint = (int(((endy-starty)-b)/a),(endy-starty))
 cap.set(3,pixelWidth)
 cap.set(4,pixelHeight)
 
+img_counter = 1
+
 while True:
-    _, image = cap.read()
+    frameCaptured, image = cap.read()
 
-            
+    if(not frameCaptured):
+        break
 
-    if image is not None:
-        image = image[starty:endy , startx:endx]
-        cv2.line(image, startLinePoint, endLinePoint, (0,0,255), 3)
-        cv2.imshow("ultrasound", image)
-        if cv2.waitKey(1) == ord('q'):
-            #exit while loop
-            break
+    #image = image[starty:endy , startx:endx]
+    #cv2.line(image, startLinePoint, endLinePoint, (0,0,255), 2)
+    cv2.imshow("ultrasound", image)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, image)
+        print("{} written!".format(img_name))
+        img_counter += 1
 
 cap.release()
 cv2.destroyAllWindows()
